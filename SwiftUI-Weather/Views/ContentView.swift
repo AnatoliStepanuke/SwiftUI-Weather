25ba5f43da5524a77287a5299bdfa3cc90cfb36a
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2025-01-28 18:23:13 +0300

    Added horizontal stack with vertical stack with detailed weather information.

--- SwiftUI-Weather/Views/ContentView.swift
+++ SwiftUI-Weather/Views/ContentView.swift
@@ -24,6 +24,23 @@ struct ContentView: View {
                         .font(.system(size: 70, weight: .medium))
                         .foregroundStyle(.white)
                 }
+                
+                HStack() {
+                    VStack(spacing: 8) {
+                        Text("TUE")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundStyle(.white)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                        Text("74°")
+                            .font(.system(size: 28, weight: .medium))
+                            .foregroundStyle(.white)
+                        
+                    }
+                }
                 Spacer()
             }
         }
