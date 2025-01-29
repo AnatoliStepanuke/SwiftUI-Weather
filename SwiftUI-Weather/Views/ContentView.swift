345b2e2e409d50df63561a9b3e18e9295c99a53f
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2025-01-29 13:37:12 +0300

    Created custom WeatherDayView struct

--- SwiftUI-Weather/Views/ContentView.swift
+++ SwiftUI-Weather/Views/ContentView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+// MARK: - ContentView
 struct ContentView: View {
     var body: some View {
         ZStack {
@@ -25,21 +26,28 @@ struct ContentView: View {
                         .foregroundStyle(.white)
                 }
                 
-                HStack() {
-                    VStack(spacing: 8) {
-                        Text("TUE")
-                            .font(.system(size: 16, weight: .medium))
-                            .foregroundStyle(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .renderingMode(.original)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 40, height: 40)
-                        Text("74°")
-                            .font(.system(size: 28, weight: .medium))
-                            .foregroundStyle(.white)
-                        
-                    }
+                Spacer()
+
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 74)
+                    
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "sun.max.fill",
+                                   temperature: 76)
+                    
+                    WeatherDayView(dayOfWeek: "THU",
+                                   imageName: "sun.max.fill",
+                                   temperature: 67)
+                    
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "wind.snow",
+                                   temperature: 55)
+                    
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "snow",
+                                   temperature: 38)
                 }
                 Spacer()
             }
@@ -49,4 +57,29 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+// MARK: - WeatherDayView
+struct WeatherDayView: View {
+    // MARK: - Variables
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium))
+                .foregroundStyle(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundStyle(.white)
+            
+        }
+    }
 }
