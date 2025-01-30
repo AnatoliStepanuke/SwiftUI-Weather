10c3d90ca9ef02fb1e4493aa573a1631ba497b64
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2025-01-30 18:35:47 +0300

    Created WeatherButton file.

--- SwiftUI-Weather/Views/ContentView.swift
+++ SwiftUI-Weather/Views/ContentView.swift
@@ -124,19 +124,3 @@ struct MainWeatherStatusView: View {
         .padding(.bottom, 40)
     }
 }
-
-// MARK: - WeatherButton
-struct WeatherButton: View {
-    var title: String
-    var textColor: Color
-    var backgroundColor: Color
-    
-    var body: some View {
-        Text(title)
-            .frame(width: 280, height: 50)
-            .foregroundStyle(textColor)
-            .background(backgroundColor)
-            .font(.system(size: 20, weight: .bold, design: .default))
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
